14c66ca5ae41156fdfac2b382a98515a4bcba4bd
cyrilbz <136728423+cyrilbz@users.noreply.github.com>
2023-06-19 15:23:57 +0200

    Update dyfun.m

--- base_case_2/dyfun.m
+++ base_case_2/dyfun.m
@@ -1,3 +1,21 @@
+% % This file is part of "Freezing Branch", a code that simulates 
+% % freeze induced cell dehydration, pressure and diameter changes 
+% % in a tree stem.
+% % 
+% % Author: Cyril Bozonnet (cyril.bozonnet@inrae.fr; github: cyrilbz) 
+% %         INRAE, PIAF, Clermont-Ferrand
+% %         
+% %         The code structure has been inspired from an existing code 
+% %         written by Isabell Graf (Konrad) and John M. Stockie
+% %         Department of Mathematics
+% %         Simon Fraser University
+% %          
+% % Developped using Matlab version R2018a.
+% % 
+% % The code is distributed under the CeCILL-B free software 
+% % license agreement.
+% % (https://cecill.info/licences/Licence_CeCILL-B_V1-en.html)
+
 %% Main function to compute dy/dt
 function dydt=dyfun(t,y,p) 
 
